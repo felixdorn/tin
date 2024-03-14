c34a626a3e2c5b056aec6ef1456eae86e59c67e8
Félix Dorn <git@felixdorn.fr>
2024-03-14 21:00:49 +0100

    docs

--- src/Contracts/OutputInterface.php
+++ src/Contracts/OutputInterface.php
@@ -7,9 +7,10 @@ use Felix\Tin\Line;
 
 interface OutputInterface
 {
-    /** Transform the token value to the desired output format with respect to the token type and theme */
+    /** Transforms the token value to the desired output format with respect to the token type and theme */
     public function transform(TokenType $type, string $value): string;
 
+    /** Transforms a line by calling the transform() method on $line->output */
     public function transformLine(Line $line): ?string;
 
     /** Returns the theme used by the output */
